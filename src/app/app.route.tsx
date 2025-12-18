ce3aa1f37a4860605dd94f5c952917670a627b16
Mikel Ken <mayki6866@gmail.com>
2025-12-18 16:49:17 -0400

    key8

--- src/app/app.route.tsx
+++ src/app/app.route.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import SigninForm from "./auth/components/SigninForm";
 import AuthRoute from "./auth/auth.route";
 import KeyForm from "./key/components/KeyForm";
